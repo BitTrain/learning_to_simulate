6b9489683015ef562560d133e0789c513abe849b
dinbergare <viktor.bergstedt.8776@student.uu.se>
2025-06-17 03:17:58 +0200

    removed debug print

--- models/learned_simulator.py
+++ models/learned_simulator.py
@@ -205,8 +205,6 @@ class LearnedSimulator(tf.keras.Model):
             else:
                 global_context = global_contexts[step + num_seed - 1][tf.newaxis]
 
-            tf.print("seed_pos shape:", tf.shape(seed_pos))
-
             # Update positions
             pred_acc = self(
                 {
